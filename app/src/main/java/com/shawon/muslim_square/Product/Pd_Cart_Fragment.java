c2b6e0c2ae6c81a2610b75456648b72a93ec07b8
Mohammad Shawon <shawonhossain965@gmail.com>
2021-07-14 12:11:11 +0600

    muslim-square

---
+++ app/src/main/java/com/shawon/muslim_square/Product/Pd_Cart_Fragment.java
@@ -0,0 +1,23 @@
+package com.shawon.muslim_square.Product;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shawon.muslim_square.R;
+
+public class Pd_Cart_Fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_pd__cart_, container, false);
+        return view;
+    }
+}
